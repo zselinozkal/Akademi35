ac0fe5b21c80b52bfc96510c113d0b22ca8b151e
Zeynep Selin Özkal <121230346+zselinozkal@users.noreply.github.com>
2025-05-05 18:04:55 +0300

    can

--- Assets/Selin/CollectibleTracker.cs
+++ Assets/Selin/CollectibleTracker.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine.UI;
+
 public static class CollectibleTracker
 {
     public static int collectedCount = 0;
-    public static int healthCount = 100;
+    public static int healthCount = 3; // 3 kalp varsayılan
 }
--- Assets/Selin/HealthS.cs
+++ Assets/Selin/HealthS.cs
@@ -1,35 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class HealthS : MonoBehaviour
 {
-
+    public List<Image> healthImage;
     private bool playerInCanavarRange = false;
-    
     private bool isInTouch = false;
-    
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
     void Update()
     {
-        if (playerInCanavarRange)
+        if (playerInCanavarRange && !isInTouch)
         {
-            isInTouch = false; // tekrar tetiklenmesini engelle
+            isInTouch = true;
+            if (CollectibleTracker.healthCount > 0)
+            {
+                CollectibleTracker.healthCount--;
+                UpdateUI();
+            }
+        }
+    }
 
-            CollectibleTracker.healthCount = CollectibleTracker.healthCount - 10;
-            FindObjectOfType<HealthUI>()?.UpdateCountText();
-            
-            
+    void UpdateUI()
+    {
+        for (int i = 0; i < healthImage.Count; i++)
+        {
+            healthImage[i].enabled = (i < CollectibleTracker.healthCount);
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Canavar1"))
+        if (other.CompareTag("Player"))
             playerInCanavarRange = true;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Canavar1"))
+        if (other.CompareTag("Player"))
+        {
             playerInCanavarRange = false;
+            isInTouch = false;
+        }
     }
-    
 }
